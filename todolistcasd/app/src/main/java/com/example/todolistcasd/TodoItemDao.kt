a2fe7538300b1344f97c7f18a0edeb4b75ab8669
Christopher Abbod <chrisabbod@gmail.com>
2023-04-02 16:28:07 -0400

    Create TodoItemRepository

--- todolistcasd/app/src/main/java/com/example/todolistcasd/TodoItemDao.kt
+++ todolistcasd/app/src/main/java/com/example/todolistcasd/TodoItemDao.kt
@@ -6,12 +6,13 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TodoItemDao {
 
     @Query("SELECT * FROM todo_item_table")
-    fun getAllTodoItems(): List<TodoItem>
+    fun getAllTodoItems(): Flow<List<TodoItem>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(todoItem: TodoItem)
