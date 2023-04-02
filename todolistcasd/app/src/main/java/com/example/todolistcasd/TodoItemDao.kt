18f50772545dc6e973119baaf9ccaeb1855bcda8
Christopher Abbod <chrisabbod@gmail.com>
2023-04-02 16:07:19 -0400

    Add Insert, Delete, and Update functions to TodoItemDao interface

--- todolistcasd/app/src/main/java/com/example/todolistcasd/TodoItemDao.kt
+++ todolistcasd/app/src/main/java/com/example/todolistcasd/TodoItemDao.kt
@@ -1,6 +1,9 @@
 package com.example.todolistcasd
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
 @Dao
@@ -8,4 +11,10 @@ interface TodoItemDao {
 
     @Query("SELECT * FROM todo_item_table")
     fun getAllTodoItems(): List<TodoItem>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(todoItem: TodoItem)
+
+    @Delete
+    suspend fun delete(todoItem: TodoItem)
 }
