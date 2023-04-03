95763ac4cbeb10ce8f7c897f64438a3be9a4d13f
Christopher Abbod <chrisabbod@gmail.com>
2023-04-02 20:22:03 -0400

    Add application scope val in TodoListApplication class

--- todolistcasd/app/src/main/java/com/example/todolistcasd/TodoListApplication.kt
+++ todolistcasd/app/src/main/java/com/example/todolistcasd/TodoListApplication.kt
@@ -1,6 +1,8 @@
 package com.example.todolistcasd
 
 import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 /**
  * Make the Database and Repository members of the application class. The reason for this is:
@@ -9,10 +11,11 @@ import android.app.Application
  * be retrieved when needed rather than constructed every time
  */
 class TodoListApplication : Application() {
+    private val applicationScope = CoroutineScope(SupervisorJob())
     /**
      * Using by lazy so the database and repository are only created when needed rather than
      * when the application starts
      */
-    private val database by lazy { TodoItemRoomDatabase.getDatabase(this) }
+    private val database by lazy { TodoItemRoomDatabase.getDatabase(this, applicationScope) }
     val repository by lazy { TodoItemRepository(database.todoItemDao())}
 }
